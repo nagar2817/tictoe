4147624cfa3f5d4aea26f3dbc222461533ed66e5
Rohit Nagar <nagar.2@iitj.ac.in>
2024-09-18 17:19:44 +0000

    build script

--- build.sh
+++ build.sh
@@ -2,6 +2,12 @@
 # exit on error
 set -o errexit
 
+# Ensure the directory for static files exists
+mkdir -p /opt/render/project/src/staticfiles
+
+# Activate the virtual environment
+source /opt/render/project/src/.venv/bin/activate
+
 pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
